bfcebefebc85fb08fa643f8d36aca90f8531f7a6
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2013-12-07 19:34:14 +0100

    Fixed django_filer base setting

--- {{cookiecutter.repo_name}}/{{cookiecutter.django_cms_app}}/{{cookiecutter.django_cms_app}}/settings/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.django_cms_app}}/{{cookiecutter.django_cms_app}}/settings/base.py
@@ -221,6 +221,6 @@ LANGUAGES = [
     {% endfor %}
 ]
 
-{%- if cookiecutter.django_filer == "y" or cookiecutter.django_filer == "Y" -%}
+{% if cookiecutter.django_filer == "y" or cookiecutter.django_filer == "Y" %}
 TEXT_SAVE_IMAGE_FUNCTION='cmsplugin_filer_image.integrations.ckeditor.create_image_plugin'
 {% endif %}
