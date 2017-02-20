29991565880faf9582b8bc6723bfb2724e292461
Contora Sebastian <sebastian@kuwaitnet.com>
2017-02-20 17:31:47 +0200

    add missing secret key in base.py

--- {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings/base.py
@@ -124,6 +124,8 @@ STATICFILES_FINDERS = (
     # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 
 )
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = environ.get('DJANGO_SECRET_KEY')
 
 # List of callables that know how to import templates from various sources.
 TEMPLATES = [
